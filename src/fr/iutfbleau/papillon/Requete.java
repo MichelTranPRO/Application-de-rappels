be1a1397e63c37972615be27575a712f0981d6e0
nossibR <rayanbisson44@gmail.com>
2025-12-05 17:11:39 +0100

    ajout template requetes je les remplirais après

--- src/fr/iutfbleau/papillon/Requete.java
+++ src/fr/iutfbleau/papillon/Requete.java
@@ -6,32 +6,43 @@ import java.util.ArrayList;
 import javax.swing.JDialog;
 
 public class Requete{
-	public static List<Rappel> getAllRappels(){
-		try {
-			Class.forName("org.mariadb.jdbc.Driver");
-			try (Connection cnx = DriverManager.getConnection(
-						"jdbc:mariadb://dwarves.iut-fbleau.fr/bissonr", "bissonr", "ananas");
-					PreparedStatement pst = cnx.prepareStatement(
-						"SELECT * FROM DEV31");
-						//"ORDER BY rang ASC");
-					ResultSet rs = pst.executeQuery()) {
+  private Connection cnx = DriverManager.getConnection(
+      "jdbc:mariadb://dwarves.iut-fbleau.fr/bissonr", "bissonr", "ananas");
+  public static List<Rappel> getAllRappels(){
+    try {
+      Class.forName("org.mariadb.jdbc.Driver");
+      try (PreparedStatement pst = cnx.prepareStatement(
+            "SELECT * FROM DEV31");
+          "ORDER BY rang ASC");
+      ResultSet rs = pst.executeQuery()) {
 
-				int rang = 1;
-				List<Rappel> results = new ArrayList<>();
-				while (rs.next()) {
-					Rappel rap = new Rappel(rs.getString("titre"), rs.getString("contenu"), rs.getInt("theme"), rang, rs.getInt("id"));
-					results.add(rap);
-					rang++; // Suivi du rang pour l'emplacement des rappels dans le layout
-				}
-				return results;
-			} catch (SQLException e2) {
-				System.err.println("Problème lié à la BD : " + e2.getMessage());
-				return new ArrayList<>();
-			}
-		} catch (ClassNotFoundException e) {
-			System.err.println("Classe pas trouvée : " + e.getMessage());
-			return new ArrayList<>();
-		}
-	}
+        int rang = 1;
+        List<Rappel> results = new ArrayList<>();
+        while (rs.next()) {
+          Rappel rap = new Rappel(rs.getString("titre"), rs.getString("contenu"), rs.getInt("theme"), rang, rs.getInt("id"));
+          results.add(rap);
+          rang++; // Suivi du rang pour l'emplacement des rappels dans le layout
+        }
+        return results;
+      } catch (SQLException e2) {
+        System.err.println("Problème lié à la BD : " + e2.getMessage());
+        return new ArrayList<>();
+      }
+    } catch (ClassNotFoundException e) {
+      System.err.println("Classe pas trouvée : " + e.getMessage());
+      return new ArrayList<>();
+    }
+  }
+  // Ne pas oublier de faire la vérification des variables au préalable
+  public static int insert(int id, String titre, String contenu, int theme, int rang){
 
+  }
+
+  public static int swap(int ida, int idb){
+
+  }
+
+  public static int delete(int ida, int idb){
+
+  }
 }
