3553db63dfd4981feb4cd8ed7b608da11f7b6854
srivasta <emmanuel.srivastava-tiamzon@etu-pec.fr>
2025-12-08 01:07:17 +0100

    Un autre controleur

---
+++ src/fr/iutfbleau/papillon/ControleurWindowConfirm.java
@@ -0,0 +1,18 @@
+package fr.iutfbleau.papillon;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class ControleurWindowConfirm extends WindowAdapter {
+
+    private FenetreMain fenetreMain;
+
+    public ControleurWindowConfirm(FenetreMain fenetreMain) {
+        this.fenetreMain = fenetreMain;
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        this.fenetreMain.afficherEcranConfirmation();
+    }
+}
